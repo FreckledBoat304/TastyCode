17aa1627df6bc47f84e746ba7d5aa8645b567ec9
gunther-ahrens <44000272+gunther-ahrens@users.noreply.github.com>
2021-05-20 15:30:26 -0500

    Update

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 window.onload = main;
-var openFiles = [{"name": "Hello", "id": getRandomID()}, {"name": "Hey", "id": getRandomID()}, {"name": "Gay", "id": getRandomID()}];
+var openFiles = [{"name": "index.html", "id": getRandomID()}, {"name": "main.css", "id": getRandomID()}, {"name": "main.js", "id": getRandomID()}];
 var activeFile = openFiles[0].id;
 var draggingTab = false;
 
@@ -9,9 +9,50 @@ function main(){
         this.scrollBy(e.deltaY, 0);
     });
 
-    document.getElementById("tabbar").addEventListener("mousemove", function(e){
+    window.addEventListener("mousemove", function(e){
         if(draggingTab){
-            
+            var relativeX = e.x - (window.innerWidth * .2) + document.getElementById("tabbar").scrollLeft;
+            var tabX = Math.round(relativeX/204)*202 - 2;
+            if(tabX < 0) tabX = 0;
+            if(tabX > (document.getElementById("tabbar").getElementsByClassName("tab").length*202)) tabX = (document.getElementById("tabbar").getElementsByClassName("tab").length*202) - 2;
+            document.getElementById("tabMarker").style.left = tabX + "px";
+        }
+    });
+
+    window.addEventListener("mouseup", function(e){
+        if(draggingTab){
+            var relativeX = e.x - (window.innerWidth * .2) + document.getElementById("tabbar").scrollLeft;
+            var tabX = Math.round(relativeX/204)*202 - 2;
+            if(tabX < 0) tabX = 0;
+            if(tabX > (document.getElementById("tabbar").getElementsByClassName("tab").length*202)) tabX = (document.getElementById("tabbar").getElementsByClassName("tab").length*202) - 2;
+            var index, currentIndex;
+            for(var i = 0;i < openFiles.length;i++){
+                if(openFiles[i].id == draggingTab){
+                    currentIndex = i;
+                    break;
+                }
+            }
+            if(tabX == 0){
+                index = 0;
+            }else{
+                index = Math.round(tabX/202);
+                if(currentIndex < index) index -= 1;
+            }
+            if(currentIndex != index){
+                var oldTabs = [];
+                for(var i = 0;i < openFiles.length;i++){
+                    if(i != currentIndex){
+                        oldTabs.push(openFiles[i]);
+                    }
+                }
+                var newOpenFiles = oldTabs.slice(0, index);
+                newOpenFiles.push(openFiles[currentIndex]);
+                newOpenFiles.concat(oldTabs.slice(index));
+                openFiles = newOpenFiles;
+                activeTab = draggingTab;
+                displayTabs();
+            }
+            draggingTab = false;
         }
     });
 
@@ -85,8 +126,9 @@ function displayTabs(){
         tab.addEventListener("click", function(e){
             selectTab(this.documentID);
         });
-        tab.addEventListener("click", function(e){
+        tab.addEventListener("mousedown", function(e){
             draggingTab = this.documentID;
+            document.getElementById("tabMarker").style.display = "block";
         });
         document.getElementById("tabbar").appendChild(tab);
 
@@ -107,6 +149,10 @@ function displayTabs(){
     addTab.style.left = String(202 * openFiles.length - 2) + "px";
     addTab.addEventListener("click", addNewTab);
     document.getElementById("tabbar").appendChild(addTab);
+
+    var tabMarker = document.createElement("div");
+    tabMarker.id = "tabMarker";
+    document.getElementById("tabbar").appendChild(tabMarker);
 
 }
 
